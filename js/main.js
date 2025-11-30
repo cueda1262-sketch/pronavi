fc9c209c971f77d3da3ae23dee128149af0c1f7e
cueda1262-sketch <c.ueda1262@gmail.com>
2025-11-30 21:30:14 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -64,6 +64,45 @@ $(function () {
       }
     });
   });
+  /*=================================================
+  フェードイン
+  ===================================================*/
+  $(window).scroll(function (){
+	$('.fadein').each(function(){
+		var elemPos = $(this).offset().top,
+		scroll = $(window).scrollTop(),
+		windowHeight = $(window).height();
+
+			if (scroll > elemPos - windowHeight + 150){
+				$(this).addClass('scrollin');
+			}
+	  });
+  });
+
+  /*=================================================
+  下線を引く
+  ===================================================*/
+  $(window).on('scroll',function(){$(".JS_ScrollAnimationItem").each(function(){
+
+   //ページの一番上から要素までの距離を取得
+   var position = $(this).offset().top;
+   
+   //スクロールの位置を取得
+   var scroll = $(window).scrollTop();
+   
+   //ウインドウの高さを取得
+   var windowHeight = $(window).height();
+   
+   //スクロール位置が要素の位置を過ぎたとき
+   if (scroll > position - windowHeight + 200 ){
+   
+   //クラス「isActive」を与える
+     $(this).addClass('isActive');
+   }
+ });
+ 
+});
+
 });
 
 
